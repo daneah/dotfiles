0b02b4def7dafc3c327110a0b8446ed371aa5a82
Dane Hillard <github@danehillard.com>
2024-11-20 21:29:16 -0500

    Make pyright compatible with python-launcher

--- nvim/lua/user/plugins/lsp.lua
+++ nvim/lua/user/plugins/lsp.lua
@@ -1,3 +1,23 @@
+local function set_python_lsp()
+    local handle = io.popen('py -c "import sys; print(sys.prefix)"', 'r')
+    local python_path = 'python'
+    if handle then
+        local result = handle:read("*a")
+        handle:close()
+        python_path = string.gsub(result, "%s+", "") .. '/bin/python'
+    end
+    local lsp_config = require('lspconfig')
+    print('Python path: ' .. python_path)
+    lsp_config.pyright.setup({
+        settings = {
+            python = {
+                pythonPath = python_path,
+            },
+        },
+    })
+end
+vim.api.nvim_create_autocmd({'BufEnter', 'BufWinEnter'}, { pattern = '*', callback = set_python_lsp })
+
 return {
     {
         'williamboman/mason.nvim',
@@ -30,7 +50,6 @@ return {
                     },
                 },
             })
-            lsp_config.pyright.setup({})
             lsp_config.yamlls.setup({})
         end
     },
